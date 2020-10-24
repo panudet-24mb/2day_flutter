a9d811d508f962b511b3369902415c25a01ecede
Panudet Panumas <panudet.panumas@gmail.com>
2020-10-24 22:08:05 +0700

    added

--- app_2day/lib/views/ui/login/loginUi.dart
+++ app_2day/lib/views/ui/login/loginUi.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
-import 'inputWidget.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  final idController = new TextEditingController();
+
+  void dispose() {
+    idController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
@@ -25,21 +36,47 @@ class Login extends StatelessWidget {
                 Stack(
                   alignment: Alignment.bottomRight,
                   children: <Widget>[
-                    InputWidget(),
+                    Padding(
+                      padding: EdgeInsets.only(right: 40, bottom: 30),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width - 40,
+                        child: Material(
+                          elevation: 10,
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(30.0),
+                                  topRight: Radius.circular(0.0))),
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                left: 40, right: 20, top: 10, bottom: 10),
+                            child: TextField(
+                              controller: idController,
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "1XX0501XX064X",
+                                  hintStyle: TextStyle(
+                                      color: Color(0xFFE1E1E1), fontSize: 14)),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                     Padding(
                         padding: EdgeInsets.only(right: 50),
                         child: Row(
                           children: <Widget>[
                             Expanded(
                                 child: Padding(
-                              padding: EdgeInsets.only(top: 40),
-                              child: Text(
-                                'กรุณากรอกบัตรประชาชนเพื่อเริ่มต้นใช้งาน...',
-                                textAlign: TextAlign.end,
-                                style: TextStyle(
-                                    color: Color(0xFFA0A0A0), fontSize: 12),
-                              ),
-                            )),
+                                  padding: EdgeInsets.only(top: 40),
+                                  child: Text(
+                                    'กรุณากรอกบัตรประชาชนเพื่อเริ่มต้นใช้งาน...',
+                                    textAlign: TextAlign.end,
+                                    style: TextStyle(
+                                        color: Color(0xFFA0A0A0), fontSize: 12),
+                                  ),
+                                )),
                             Container(
                                 padding: EdgeInsets.all(10),
                                 child: ClipOval(
@@ -50,8 +87,13 @@ class Login extends StatelessWidget {
                                       child: SizedBox(
                                           width: 56,
                                           height: 56,
-                                          child: Icon(Icons.arrow_forward_ios_outlined, color:Colors.white )),
-                                      onTap: () {},
+                                          child: Icon(
+                                              Icons.arrow_forward_ios_outlined,
+                                              color: Colors.white)),
+                                      onTap: () {
+                                        var id = idController.text;
+                                        print(id);
+                                      },
                                     ),
                                   ),
                                 )),
